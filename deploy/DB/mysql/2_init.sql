9142e1b57fd7bd5aa38c86b6f0a36675f8a7969c
xiyang <90125263+JBossBC@users.noreply.github.com>
2023-01-13 15:21:34 +0800

    update fileserver dockerfile

---
+++ deploy/DB/mysql/2_init.sql
@@ -0,0 +1,4 @@
+
+use cloudStorageSystem;
+insert into FileMetaTable values("xiyang","xiyang","hello","this is test file",NOW(),"644","file",NOW(),0,0,null);
+insert into usergroup values("xiyang","xiyang");
